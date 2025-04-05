82997860d45a00afac31c958440189a0f8977f07
OnCloud <anonymousaaaa41414141@gmail.com>
2025-04-06 00:08:20 +0800

    refactor: move error handling logic to separate module

---
+++ modules/fastify/plugins/errorHandlers.ts
@@ -0,0 +1,44 @@
+import { FastifyError, FastifyReply, FastifyRequest } from "fastify";
+
+import { HttpStatus } from "../../http/statusCodes";
+import { ClientError } from "../../clientError";
+
+export function notFoundHandler(
+  _request: FastifyRequest,
+  response: FastifyReply,
+) {
+  return response.status(HttpStatus.NOT_FOUND).send({
+    errorMessage: "Invalid request path or method.",
+  });
+}
+
+export function fallbackErrorHandler(
+  error: FastifyError,
+  _request: FastifyRequest,
+  response: FastifyReply,
+) {
+  switch (true) {
+    case error.code === "FST_ERR_VALIDATION": {
+      return response.status(HttpStatus.BAD_REQUEST).send({
+        errorMessage: error.message,
+        errorObject: {
+          validationErrors: error.validation,
+        },
+      });
+    }
+
+    case "__type__" in error && error.__type__ === "CLIENT_ERROR": {
+      const clientError = error as unknown as ClientError;
+      return response.status(clientError.code).send({
+        errorMessage: clientError.payload?.errorMessage,
+        errorObject: clientError.payload?.errorObject,
+      });
+    }
+
+    default: {
+      return response.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+        errorMessage: "Unexpected error occurred. Please try again.",
+      });
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import fastifyEnv from "@fastify/env";
 
 import { envOptions } from "../modules/fastify/envOptions";
 import { appRouter } from "./app/router";
-import { ClientError } from "../modules/clientError";
-import { HttpStatus } from "../modules/http/statusCodes";
+import {
+  fallbackErrorHandler,
+  notFoundHandler,
+} from "../modules/fastify/plugins/errorHandlers";
 
 export const fastifyApp = Fastify({
   logger: {
@@ -21,38 +23,9 @@ fastifyApp.register(fastifyEnv, envOptions).ready((_error) => {
   );
 });
 
-fastifyApp.setNotFoundHandler((_request, reply) => {
-  reply.status(HttpStatus.NOT_FOUND).send({
-    errorMessage: "Invalid request path or method.",
-  });
-});
+fastifyApp.setNotFoundHandler(notFoundHandler);
 
-fastifyApp.setErrorHandler((error, _request, reply) => {
-  switch (true) {
-    case error.code === "FST_ERR_VALIDATION": {
-      return reply.status(HttpStatus.BAD_REQUEST).send({
-        errorMessage: error.message,
-        errorObject: {
-          validationErrors: error.validation,
-        },
-      });
-    }
-
-    case "__type__" in error && error.__type__ === "CLIENT_ERROR": {
-      const clientError = error as unknown as ClientError;
-      return reply.status(clientError.code).send({
-        errorMessage: clientError.payload?.errorMessage,
-        errorObject: clientError.payload?.errorObject,
-      });
-    }
-
-    default: {
-      return reply.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
-        errorMessage: "Unexpected error occurred. Please try again.",
-      });
-    }
-  }
-});
+fastifyApp.setErrorHandler(fallbackErrorHandler);
 
 appRouter(fastifyApp);
 
